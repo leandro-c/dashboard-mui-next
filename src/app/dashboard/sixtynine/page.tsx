0a586642468a2d3ab4e2d840b7c7a1c3398d7edb
Leandro Cesar Casarin <lccasarin@HL-PF40LCWXC94.local>
2024-05-08 09:34:44 +0200

    fix set up

---
+++ src/app/dashboard/sixtynine/page.tsx
@@ -0,0 +1,11 @@
+import Sixtynine from 'src/sections/sixtynine/view';
+
+// ----------------------------------------------------------------------
+
+export const metadata = {
+  title: 'Dashboard: One',
+};
+
+export default function Page() {
+  return <Sixtynine />;
+}
